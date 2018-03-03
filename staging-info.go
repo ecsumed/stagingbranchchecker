42d01bc6b14c6e92e55189cb49f53abb6eb63594
Fahad Saleh <fahadsaleh.py@gmail.com>
2018-03-03 12:06:20 +0500

    Init

---
+++ staging-info.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "fmt"
+    git "gopkg.in/src-d/go-git.v4"
+)
+
+// type Clock struct {
+//     hour int
+//     minute int
+// }
+// 
+// func New(hour, minute int) Clock {
+//     var minutes int
+//     var hours int
+//     
+//     minutes = 60 + minute
+//     minutes = minutes % 60
+// 
+//     hours = 24 + hour
+//     hours = hours + (minute / 60)
+//     hours = hours % 24
+// 
+//     return Clock{hour: hours, minute: minutes}
+// }
+// 
+// func (c Clock) String() string {
+//     return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+// }
+// 
+// func (c Clock) Add(minutes int) Clock {
+//     c.minute = c.minute + minutes
+//     
+//     return Clock{hour: c.hour, minute: c.minute}
+// }
+
+func main() {
+	repo_path := "/home/fahad/Workspace/go/src/staging-info"
+	repo, _ := git.PlainOpen(repo_path)
+
+	head, _ := repo.Head()
+	fmt.Printf("%s", head)
+
+	// worktree, _ := repo.Worktree()
+	// wt_status, _ := worktree.Status()
+	// fmt.Printf("%s", wt_status)
+}
